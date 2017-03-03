8145e6b6d9679a543a089041fd9c23f44988f35e
Navakanth <navakanthtunga@gmail.com>
2017-03-03 16:01:20 +0530

    for sauce
    03:03:2017

--- src/test/java/com/library/Baselib.java
+++ src/test/java/com/library/Baselib.java
@@ -11,17 +11,17 @@ import org.testng.annotations.BeforeMethod;
 
 public class Baselib {
 	public static WebDriver driver;
-	public static final String USERNAME = "shradhanjalidalal";
-	public static final String ACCESS_KEY = "4778fe48-9a83-4044-a236-0de35a9ab475";
+	public static final String USERNAME = "navakanthtunga1258";
+	public static final String ACCESS_KEY = "c250c622-7bbc-4f1f-8fe0-0da2656b5e6d";
 	public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
 	
 @BeforeMethod
 
 public void setUp() throws MalformedURLException{
 	DesiredCapabilities caps = DesiredCapabilities.firefox();
-    caps.setCapability("platform", "Windows 8");
-    caps.setCapability("version", "51.0");
-    caps.setCapability("name","Voonik Sample Test");
+/*    caps.setCapability("platform", "Windows 7");
+    caps.setCapability("version", "51.0");*/
+    caps.setCapability("name","demo site");
     driver = new RemoteWebDriver(new URL(URL),caps);
 }
 public void settings(){
